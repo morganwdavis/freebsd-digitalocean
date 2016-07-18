ade9d17ca2171dbca35705fbe36ed87bfcf395ff
Morgan Davis <mdavis@dtl.net>
2016-07-18 13:56:59 -0700

    Add install.sh installer

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+prefix=/usr/local
+
+echo "--------------------------------------"
+echo
+echo "Installing digitalocean"
+echo
+echo "Edit configuration settings in $prefix/etc/digitalocean.conf"
+
+install -m 700 digitalocean $prefix/etc/rc.d
+install -m 700 digitalocean.sh $prefix/sbin
+
+if [ ! -f $prefix/etc/digitalocean.conf ] ; then
+	install -m 644 digitalocean.conf $prefix/etc
+else
+	if [ ! -z "`diff digitalocean.conf $prefix/etc/digitalocean.conf`" ] ; then
+		install -m 644 digitalocean.conf $prefix/etc/digitalocean.conf.sample
+		echo "Review default settings in $prefix/etc/digitalocean.conf.sample"
+	fi
+fi
+
+if [ -z "`grep 'digitalocean=' /etc/rc.conf`" ] ; then
+	echo
+	echo "To enable, add this to /etc/rc.conf"
+	echo
+	echo '	digitalocean="YES"'
+fi
+
+echo
+echo "--------------------------------------"
