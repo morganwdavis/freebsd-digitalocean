f1b8362df5b1c325b158281b3bb1f02215462860
sko <sebastian@rostwald.de>
2018-09-04 18:58:30 +0200

    fix hostname; add newline after pubkey

--- digitalocean.sh
+++ digitalocean.sh
@@ -148,8 +148,8 @@ if [ ! -z $droplet_user ] ; then
 		fi
 		me=`basename $0`
 		do_keys_tmp=`mktemp -t $me` || exit 1
-		$api_item/public-keys > $do_keys_tmp || (rm $do_keys_tmp && exit 1)
-		auth_keys="${ssh_dir}/.authorized_keys"
+		$api_item/public-keys > $do_keys_tmp && echo "" >> $do_keys_tmp || (rm $do_keys_tmp && exit 1)
+		auth_keys="${ssh_dir}/authorized_keys"
 		cat $auth_keys >> $do_keys_tmp 2>/dev/null
 		sort -u $do_keys_tmp > $auth_keys
 		rm $do_keys_tmp
